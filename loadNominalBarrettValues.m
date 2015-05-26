a8ab73c6eef216aaaf89afffea3ba6143f9f757b
Okan <okan.koc@tuebingen.mpg.de>
2015-05-26 14:26:58 +0200

    updated model parameters

--- loadNominalBarrettValues.m
+++ loadNominalBarrettValues.m
@@ -105,7 +105,93 @@ links(7).inertia(3,3) = 0.00007408;
 
 %% Load nominal robot dynamics parameters from wam.conf.flt
 
-
+link0.m = 0.0000;
+link0.mcm(1) = 0.0000;
+link0.mcm(2) = 0.0000;
+link0.mcm(3) = 0.0000;
+link0.inertia(1,1) = 0.0000; 
+link0.inertia(1,2) = 0.0000; 
+link0.inertia(1,3) = 0.0000; 
+link0.inertia(2,2) = 0.0000; 
+link0.inertia(2,3) = 0.0000;
+link0.inertia(3,3) = 0.0000;                             
+% SFE joint
+links(1).m = 8.3936; 
+links(1).mcm(1) = 0.002942796;   
+links(1).mcm(2) = 1.11365865;    
+links(1).mcm(3) = 0.00527621696;
+links(1).inertia(1,1) = 95157.4294e-6;    
+links(1).inertia(1,2) = 246.1404e-6;  
+links(1).inertia(1,3) = -95.0183e-6;  
+links(1).inertia(2,2) = 92032.3524e-6;    
+links(1).inertia(2,3) = -962.6725e-6;  
+links(1).inertia(3,3) = 59290.5997e-6;
+% SAA joint
+links(2).m = 4.8487;
+links(2).mcm(1) = -0.00108126;   
+links(2).mcm(2) = -0.10372533;    
+links(2).mcm(3) = 0.064853302;
+links(2).inertia(1,1) = 29326.8098e-6;  
+links(2).inertia(1,2) = -43.3994e-6; 
+links(2).inertia(1,3) = -129.2942e-6;  
+links(2).inertia(2,2) = 20781.5826e-6;    
+links(2).inertia(2,3) = 1348.6924e-6;  
+links(2).inertia(3,3) =  22807.3271e-6;         
+% HR joint    
+links(3).m = 1.7251;
+links(3).mcm(1) = -0.066858838;    
+links(3).mcm(2) = 0.37591274578;     
+links(3).mcm(3) = 4.347252e-05;
+links(3).inertia(1,1) = 56662.2970e-6;    
+links(3).inertia(1,2) = -2321.6892e-6;    
+links(3).inertia(1,3) = 8.2125e-6;  
+links(3).inertia(2,2) = 3158.0509e-6; 
+links(3).inertia(2,3) = -16.6307e-6;  
+links(3).inertia(3,3) = 56806.6024e-6;
+% EB joint (elbow)
+links(4).m = 2.17266212;                        
+links(4).mcm(1) =  0.012023686;
+links(4).mcm(2) = 1.482190e-04;
+links(4).mcm(3) = 0.259148511; 
+links(4).inertia(1,1) = 0.01067491;
+links(4).inertia(1,2) = 0.00004503;
+links(4).inertia(1,3) = -0.00135557;
+links(4).inertia(2,2) = 0.01058659;
+links(4).inertia(2,3) = -0.00011002;
+links(4).inertia(3,3) = 0.00282036;
+% WR joint (wrist 1)
+links(5).m = 0.35655692;                    
+links(5).mcm(1) = 1.9550015923e-05;
+links(5).mcm(2) = 0.01029125; 
+links(5).mcm(3) = 5.2946206721e-04; 
+links(5).inertia(1,1) = 0.00037112;
+links(5).inertia(1,2) = -0.00000008;
+links(5).inertia(1,3) = -0.00000003;
+links(5).inertia(2,2) = 0.00019434;
+links(5).inertia(2,3) = -0.00001613;
+links(5).inertia(3,3) = 0.00038209;
+% WFE joint (wrist 2)
+links(6).m = 0.40915886;                   
+links(6).mcm(1) = -2.4234479e-05; 
+links(6).mcm(2) = -0.00689892;  
+links(6).mcm(3) = 0.00989776; 
+links(6).inertia(1,1) = 0.00054889; 
+links(6).inertia(1,2) = 0.00000019; 
+links(6).inertia(1,3) = -0.00000010; 
+links(6).inertia(2,2) = 0.00023846; 
+links(6).inertia(2,3) = -0.00004430;
+links(6).inertia(3,3) = 0.00045133;
+% WAA joint (wrist 3)     
+links(7).m = 0.07548270; 
+links(7).mcm(1) = 1.119861e-05;  
+links(7).mcm(2) = 5.4740e-06;  
+links(7).mcm(3) = -2.5300668799e-04;  
+links(7).inertia(1,1) = 0.00003911; 
+links(7).inertia(1,2) = 0.00000019;
+links(7).inertia(1,3) = 0.00000000;  
+links(7).inertia(2,2) = 0.00003877; 
+links(7).inertia(2,3) = 0.00000000;  
+links(7).inertia(3,3) = 0.00007614;
 
 %% Organize values
 % make sure inertia matrices are symmetric
