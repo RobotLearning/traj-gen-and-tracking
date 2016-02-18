efd29151218cce9b0e5556801fbb85008b31f935
Okan <okankoc@gmail.com>
2016-02-18 11:16:13 +0100

    Update tableTennis3D.m

--- tableTennis3D.m
+++ tableTennis3D.m
@@ -1,17 +1,2 @@
-%% 3D table tennis with 2 Barrett WAMs
-
-clc; clear; close all; dbstop if error;
-
-% VHP method vs. optimal control approach
-
-profile on;
-initializeWAM;
-% change base for wam2
-wam2 = [];
-% For a match run
-% tt.match();
-tt = TableTennis(wam,wam2);
-tt.match();
-profile viewer
 
 
