9fe76cc76a20ce71cf7f6116024e142169517403
okan <okankoc@gmail.com>
2016-03-28 18:14:29 +0200

    revised README

--- tableTennisPractice.m
+++ tableTennisPractice.m
@@ -9,11 +9,11 @@ OPT.train = false; % train a lookup table using optimization results
 OPT.lookup = true; % use lookup table instead of optimizing online
 OPT.vhp = false; % use vhp strategy
 % initial ball pos and vel standard deviation
-std.pos = 0.1;
-std.vel = 0.1;
+STD.pos = 0.1;
+STD.vel = 0.1;
 % measurement standard deviation 
-std.camera = 0.0;
-tt = TableTennis(wam,wam2,q0,std,OPT);
+STD.camera = 0.0;
+tt = TableTennis(wam,wam2,q0,STD,OPT);
 tt.practice(q0,1);
 
 % Things to do for simulation
