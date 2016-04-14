683effe20981988cd57cf578321ef3d3e2c879f9
Okan Koc <okoc@blies.is.localnet>
2016-04-14 19:29:27 +0200

    working on ball estimation

--- train/analyzeFilterOnRealBallData.m
+++ train/analyzeFilterOnRealBallData.m
@@ -48,74 +48,122 @@ mats.C = C;
 mats.M = eps * eye(3);
 filter = EKF(dim,funState,mats);
 
-%% Get the number of incoming balls 
+%% Get reliable ball observations
 % this is to throw away really bad observations as to not confuse the
 % filter
 zMax = 0.5;
+
 % camera 1 time indices for reliable observations
 idx1 = find(B(:,2) == 1 & B(:,5) >= table_z & B(:,5) < zMax);
 t1 = t(idx1);
-Mball1 = B(:,3:5);
-bx1 = Mball1(idx1,1);
-by1 = Mball1(idx1,2);
-bz1 = Mball1(idx1,3);
+% order the ball data w.r.t. time and throw away same values
+b1 = [t1,B(idx1,3:5)];
+b1 = sortrows(b1);
+j = 1; % time from ballgun to net 
+tol = 1e-3;
+idxDiffBallPos1 = []; % index for diff ball positions
+for i = 1:size(b1,1)-1
+    if norm(b1(i,2:4) - b1(i+1,2:4)) > tol
+        idxDiffBallPos1(j) = i;
+        j = j+1;
+    end
+end
+b1 = b1(idxDiffBallPos1,:);
 
 % camera 3 time indices for reliable observations
 idx3 = find(B(:,7) == 1 & B(:,10) >= table_z & B(:,10) < zMax);
 t3 = t(idx3);
-
 % order the ball data w.r.t. time and throw away same values
 b3 = [t3,B(idx3,8:10)];
 b3 = sortrows(b3);
-
 j = 1; % time from ballgun to net 
 tol = 1e-3;
-idxDiffBallPos = []; % index for diff ball positions
+idxDiffBallPos3 = []; % index for diff ball positions
 for i = 1:size(b3,1)-1
     if norm(b3(i,2:4) - b3(i+1,2:4)) > tol
-        idxDiffBallPos(j) = i;
+        idxDiffBallPos3(j) = i;
         j = j+1;
     end
 end
+b3 = b3(idxDiffBallPos3,:);
 
-b3 = b3(idxDiffBallPos,:);
+%% Get the number of incoming balls 
 
 % if there is more than 1 second difference it means its a new trial
 diffBall3 = diff(b3);
-idxStart = find(diffBall3(:,1) > 1.0);
-idxStart = [1;idxStart+1];
-tStart = b3(diffBall3(:,1) > 1.0,1);
-numTrials = length(idxStart);
+idxStart3 = find(diffBall3(:,1) > 1.0);
+idxStart3 = [1;idxStart3+1];
+tStart3 = b3(idxStart3,1);
+numTrials = length(idxStart3);
 
 %% Predict ball using estimate (SL uses it for lookup table)
 
-trial = 1;
+trial = 50;
 
-b3Pred = [t3,B(idx3,12:17)];
-b3Pred = sortrows(b3Pred);
-b3Pred = b3Pred(idxDiffBallPos,:);
+b3est = [t3,B(idx3,12:17)];
+b3est = sortrows(b3est);
+b3est = b3est(idxDiffBallPos3,:);
 
-ballPred = b3Pred(idxStart(trial):idxStart(trial+1)-1,:);
+ballEst = b3est(idxStart3(trial):idxStart3(trial+1)-1,:);
 % remove first 12 entries
-ballPred = ballPred(12+1:end,:);
+ballEst = ballEst(12+1:end,:);
 
 toly = 0.4;
 
 % particular rule implemented in lookup table in SL
-idxPred = find(ballPred(:,3) > yCenter & ballPred(:,3) < yCenter + toly & ...
-               ballPred(:,6) > 0.5,1);
+idxPred = find(ballEst(:,3) > yCenter & ballEst(:,3) < yCenter + toly & ...
+               ballEst(:,6) > 0.5,1);
 
-ballLookUp = ballPred(idxPred,2:end);
+ballLookUp = ballEst(idxPred,2:end);
+tLookUp = ballEst(idxPred,1);
+
+% get the indices for plotting
+b3plot = b3(idxStart3(trial):idxStart3(trial+1)-1,2:4);
+t3plot = b3(idxStart3(trial):idxStart3(trial+1)-1,1);
+b1plot = b1(b1(:,1) >= tStart3(trial) & b1(:,1) < tStart3(trial+1),2:4);
+t1plot = b1(b1(:,1) >= tStart3(trial) & b1(:,1) < tStart3(trial+1),1);
+
+% remove outliers in b1plot
+b1plot = b1plot(
+
+% find the index at bounce
+[~,idxBounce] = min(b3plot(:,3));
+
+dtPredTillBounce =  t3plot(idxBounce) - tLookUp;
+dtPredTillLastBlob3 = t3plot(end) - tLookUp;
+dtPredTillLastBlob1 = t1plot(end) - tLookUp;
 
 % predict using ball estimate
-dt = 0.01;
+dt = 1/60;
 initVar = 1;
 filter.initState(ballLookUp(:),initVar);
 filter.linearize(dt,0);
-predictHorizon = 0.8;
+predictHorizon = dtPredTillLastBlob3; % only predict till last blob3
 table.DIST = dist_to_table;
 table.LENGTH = table_length;
 table.Z = table_z;
 table.WIDTH = table_width;
 [ballPred,ballTime,numBounce,time2PassTable] = ...
             predictBallPath(dt,predictHorizon,filter,table);
+        
+%% Plot predictions
+
+figure;
+s1 = scatter3(b1plot(:,1),b1plot(:,2),b1plot(:,3),'r');
+hold on;
+s3 = scatter3(b3plot(:,1),b3plot(:,2),b3plot(:,3),'b');
+sP = scatter3(ballPred(1,:),ballPred(2,:),ballPred(3,:),'y');
+
+title('Predicted ball trajectory');
+grid on;
+axis equal;
+xlabel('x');
+ylabel('y');
+zlabel('z');
+fill3(T(1:4,1),T(1:4,2),T(1:4,3),[0 0.7 0.3]);
+fill3(net(:,1),net(:,2),net(:,3),[0 0 0]);
+s1.MarkerEdgeColor = s1.CData; % due to a bug in MATLAB R2015b
+s3.MarkerEdgeColor = s3.CData;
+sP.MarkerEdgeColor = sP.CData;
+legend('cam1','cam3','filter');
+hold off;
