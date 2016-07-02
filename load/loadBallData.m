145306509006b5f1ac915768f8ce066030d814c8
Okan Koc <okoc@blies.is.localnet>
2016-07-02 20:05:19 +0200

    working on ball data

---
+++ load/loadBallData.m
@@ -0,0 +1,15 @@
+
+function [t,B] = loadBallData(dataSet)
+
+dataSet = 1;
+file = ['../Desktop/data/realBallsimRobotData',int2str(dataSet)];
+M = dlmread([file,'.txt']);
+% ball data
+B = M(1:2:end,:);
+% robot joint data
+N_DOFS = 7;
+Q = M(2:2:end,1:2*N_DOFS);
+scale = 0.001; % recorded in milliseconds
+t = scale * B(:,11);
+
+end
