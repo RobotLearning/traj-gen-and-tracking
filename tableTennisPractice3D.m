6a3f1a900bd33085f08e189f4eb78c36edc4f184
Okan Koc <okoc@sill.is.localnet>
2017-06-26 19:03:19 +0200

    Building a lookup table based on FOCUSED player optimization outcome 3 lookup tables are needed, one for each robot initial posture

--- tableTennisPractice3D.m
+++ tableTennisPractice3D.m
@@ -3,27 +3,24 @@
 % original seed was 1
 clc; clear; close all; %rng(5);
 std = 0.05;
-robot_side = 'CENTRE';
-ballgun_side = 'CENTRE';
+robot_side = 'RIGHT';
 initializeWAM;
-[b0, v0] = initBallGun(std,ballgun_side);
 
 % drawing related params
-OPT.draw = true; % draw the simulation
+OPT.draw = false; % draw the simulation
 OPT.record = false; % record the simulation
 
 % planning related flags and parameters
-OPT.plan.method = 'LAZY'; % FOCUSED , VHP, LAZY
+OPT.plan.method = 'FOCUSED'; % FOCUSED , VHP, LAZY
 OPT.plan.vhp.y = -0.6;
 OPT.train = false; % train a lookup table using optimization results
 OPT.lookup.flag = false; % use lookup table instead of optimizing online
 OPT.lookup.mode = 'closest'; %'GP-regress'; %'lin-regress';
-OPT.lookup.savefile = 'LookupTable-16-May-2016'; %['LookupTable-', date, '.mat'];
+OPT.lookup.savefile = 'center_lookup'; %'LookupTable-16-May-2016'; 
 
 % initial ball parameters
 OPT.distr.type = 'normal';
-OPT.distr.init.mean = [b0; v0];
-OPT.distr.init.cov = std^2; 
+OPT.distr.init.std = std;
 %{
 % OPT.distr.type = 'empirical';
 % OPT.distr.data = ballTrain.bInit;
@@ -39,4 +36,4 @@ OPT.camera.cov = 0.0; %5e-4;
 % sampling time
 dt = 0.01; 
 tt = TableTennis3D(wam,dt,q0,OPT);
-tt.practice(q0,1);
+tt.practice(q0,10);
